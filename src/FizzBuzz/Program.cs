32e7250964fdc2c0ce720e3f295f1d2cd3664d56
alcavedon <alcavedon@gmail.com>
2017-02-03 14:00:32 -0500

    FizzBuzz test app

---
+++ src/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var msg = "0";
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    msg = i + " (FizzBuzz)";
+                }
+                else if (i % 3 == 0)
+                {
+                    msg = i + " (Fizz)";
+                }
+                else if (i % 5 == 0)
+                {
+                    msg = i + " (Buzz)";
+                }
+                else
+                {
+                    msg = i.ToString();
+                }
+                Console.WriteLine(msg);
+            }
+
+            Console.Read();
+        }
+    }
+}
